1b11ed040faef711716091f65039caddb3449b03
haohann <71618864+haniaapn@users.noreply.github.com>
2023-05-30 10:57:41 +0700

    add,update - new page & app bar

---
+++ app/src/main/java/com/herbify/herbifyapp/ui/herbal_pedia/BrewPageActivity.kt
@@ -0,0 +1,11 @@
+package com.herbify.herbifyapp.ui.herbal_pedia
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BrewPageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_brew_page)
+    }
+}
